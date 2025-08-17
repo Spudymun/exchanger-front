3729bc6c34211e550a60cb636d72b0707f77c072
Andrey Naboka <spudymun@gmail.com>
2025-08-18 02:30:21 +0300

    fix: 🧪 Exclude dist/ folder from Jest test discovery
    - Added '<rootDir>/dist/' to testPathIgnorePatterns
    - Prevents Jest from running empty .d.ts files as tests
    - All UI tests now pass correctly

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,29 +1,32 @@
 /* eslint-env node */
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    transform: {
-        '^.+\\.(ts|tsx)$': ['ts-jest', {
-            useESM: true,
-        }],
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    extensionsToTreatAsEsm: ['.ts', '.tsx'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.turbo/'],
-    collectCoverageFrom: [
-        'src/**/*.{ts,tsx}',
-        '!src/**/*.d.ts',
-        '!src/**/*.stories.{ts,tsx}',
-        '!src/stories/**',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
     ],
-    coverageThreshold: {
-        global: {
-            branches: 70,
-            functions: 70,
-            lines: 70,
-            statements: 70,
-        },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.turbo/', '<rootDir>/dist/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{ts,tsx}',
+    '!src/stories/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
-}
+  },
+};
